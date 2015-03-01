f70cf1ec545a91bd9b0960a8a2f1ff878364bb68
Joseph Knowles <joejknowles@gmail.com>
2015-03-01 21:55:31 +0000

    update multi matrix to first fully working version on 3x3 matrix

--- mult-matrix.rb
+++ mult-matrix.rb
@@ -1,23 +1,32 @@
 def matrix_mult(a, b)
+  count = 0
   n = a.length
-  area = n*n
   final = []
   c, d, e, f, g, h =0,0,0,0,0,0
   n.times { final << [] }
   final.each{|line| n.times{ line << 0}}
-
   n.times do
     n.times do
-      final[c][d] += a[e][f] * b[g][h]
-      f+=1
-      g+=1
+      n.times do
+        puts "this is #{final} now, at count: #{count} c:#{c} d:#{d} e:#{e} f:#{f}"
+        count += 1
+        final[c][d] += a[e][f] * b[g][h]
+        f+=1
+        g+=1
+
+      end
+      d+=1
+      h+=1
+      f = 0 if f % n == 0
+      g = 0 if g % n == 0
+      h = 0 if h % n == 0
+      d = 0 if d % n == 0
     end
-    d+=1
-    h+=1
-    f = 0 if f % n == 0
-    g = 0 if g % n == 0
-    h = 0 if h % n == 0
+
+    e += 1
+    c += 1
     c = 0 if c % n == 0
+    e = 0 if e % n == 0
   end
 
 =begin
@@ -38,6 +47,6 @@ end
 
 #puts "When n is four this method returns #{matrix_mult([[1, 2], [3, 2]], [[3, 2], [1, 1]])} which should be [[5, 4], [11, 8]] and means it works? #{matrix_mult([[1, 2], [3, 2]], [[3, 2], [1, 1]]) == [[5, 4], [11, 8]]}"
 
-puts "When n is four this method returns #{matrix_mult([[1, 2, 3], [4, 5, 6], [7, 8 ,9]], [[1, 0, 0], [0, 1, 0], [0,0,1]])} which should be [[1, 2, 3], [4, 5, 6], [7, 8 ,9]] and means it works? #{matrix_mult([[1, 2, 3], [4, 5, 6], [7, 8 ,9]], [[1, 0, 0], [0, 1, 0], [0,0,1]]) == [[1, 2, 3], [4, 5, 6], [7, 8 ,9]]}"
+#puts "When n is four this method returns #{matrix_mult([[1, 2, 3], [4, 5, 6], [7, 8 ,9]], [[1, 0, 0], [0, 1, 0], [0,0,1]])} which should be [[1, 2, 3], [4, 5, 6], [7, 8 ,9]] and means it works? #{matrix_mult([[1, 2, 3], [4, 5, 6], [7, 8 ,9]], [[1, 0, 0], [0, 1, 0], [0,0,1]]) == [[1, 2, 3], [4, 5, 6], [7, 8 ,9]]}"
 
 puts "When n is four this method returns #{matrix_mult([[2, 0, -1], [3, 5, 2], [-4, 1 ,4]], [[5, 1, -2], [-1, 0, 4], [2,-3,3]])} which should be [[8, 5, -7], [14, -3, 20], [-13, -16 ,24]] and means it works? #{matrix_mult([[2, 0, -1], [3, 5, 2], [-4, 1 ,4]], [[5, 1, -2], [-1, 0, 4], [2,-3,3]]) == [[8, 5, -7], [14, -3, 20], [-13, -16 ,24]]}"
