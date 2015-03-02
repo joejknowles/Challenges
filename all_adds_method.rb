79621bc0050e3f3e6ad9558561fbd629c897dc0c
Joseph Knowles <joejknowles@gmail.com>
2015-03-02 21:34:49 +0000

    Solve calculator for addition and subtraction

--- all_adds_method.rb
+++ all_adds_method.rb
@@ -2,31 +2,11 @@ class Calculator
   def evaluate(string)
 
     array = string.split(' ')
-    array = all_mults(array)
-    all_adds(array)
+    array = all_adds(array)
+    array
     #string[0].to_i
   end
 
-  def all_mults(array)
-    new_array = array.each_with_index.inject([]) do |new_array, (character, position)|
-      if character != "*" && array[position-1] != "*" && array[position+1] != "*" &&  character != "/" && array[position-1] != "/" && array[position+1] != "/"
-        new_array << character
-      elsif character == '*' && (array[position - 2] == '*' || array[position - 2] == '/')
-        new_array << character
-        new_array << array[position + 1]
-      elsif character == '*'
-        new_array << (array[position-1].to_i * array[position+1].to_i).to_s
-      elsif character == '/' && (array[position - 2] == '*' || array[position - 2] == '/')
-        new_array << character
-        new_array << array[position + 1]
-      elsif character == '/'
-        new_array << (array[position-1].to_i / array[position+1].to_i).to_s
-      end
-      new_array
-    end
-    new_array.join.scan('*').length > 0 || new_array.join.scan('/').length > 0 ? all_mults(new_array) : new_array
-  end
-end
 
   def all_adds(array)
     new_array = array.each_with_index.inject([]) do |new_array, (character, position)|
@@ -47,6 +27,7 @@ end
     end
     new_array.join.scan('+').length > 0 || new_array.join.scan('-').length > 0 ? all_adds(new_array) : new_array
   end
+end
 
 
 puts "calc evaluates to:\n#{Calculator.new.evaluate("2 / 2 + 3 * 4 / 6 * 5 / 5")}"
