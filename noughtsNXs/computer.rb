1e26a1ddcf0d3543ab997d36369f176c4e815205
Joseph Knowles <joejknowles@gmail.com>
2015-03-10 20:14:27 +0000

    Refactor winning moves checker in AI

--- noughtsNXs/computer.rb
+++ noughtsNXs/computer.rb
@@ -30,33 +30,30 @@ class Computer < Player
 
     # This part checks for any winning moves
     [ 5, 1, 3, 7, 9, 2, 4, 6, 8].each do |position|
-      #Along the diagonals (requires being on the correct diagonal)
-      if position % 2 == 1 && board.get_cell(position).mark == ' ' && (((board.grid.each_with_index.map{|row, index| row[index]}.count{ |mark| mark.to_s == @x_or_o} == 2) && (position == 1 || position == 9)) || ((board.grid.each_with_index.map{|row, index| row[2-index]}.count{ |mark| mark.to_s == @x_or_o} == 2) && (position == 3 || position == 7)))
+      cell = board.get_cell(position)
+      if cell.to_s == ' ' && relevant_lines(cell, winning_lines).any?{ |line| line.count{ |mark| mark.to_s == @x_or_o} == 2}
         puts 1
-        choice = position
-        break
-      # On the two horizontal/vertical lines
-      elsif board.get_cell(position).mark == ' ' && ((board.grid[(position -1) / 3].count{|mark| mark.to_s == @x_or_o} == 2) || (board.grid.transpose[(position -1) % 3].count{|mark| mark.to_s == @x_or_o} == 2) )
-        puts 2
         choice = position
         break
       end
     end
+
     # checks for any line with one of own mark and two blanks
     unless choice
+
       #diags
-      [5, 1, 3, 7, 9, 2, 4, 6, 8].each do |position|
-        if position % 2 == 1 && board.get_cell(position).mark == ' '    && ((board.grid.each_with_index.map{|row, index| row[index]}.any?{ |mark| mark.to_s == @x_or_o} && (position == 1 || position == 9) && (board.grid.each_with_index.map{|row, index| row[index]}.count{|mark| mark.to_s == ' '} == 2)) || (board.grid.each_with_index.map{|row, index| row[2-index]}.any?{ |mark| mark.to_s == @x_or_o} && (position == 3 || position == 7) && (board.grid.each_with_index.map{|row, index| row[2-index]}.count{|mark| mark.to_s == ' '} == 2)))
-          puts 3
-        choice = position
-        break
-        #hor vert
-        elsif board.get_cell(position).mark == ' ' && (board.grid[(position -1) / 3].any?{|mark| mark.to_s == @x_or_o} || board.grid.transpose[(position -1) % 3].any?{|mark| mark.to_s == @x_or_o}  ) && ((board.grid[(position -1) / 3].count{|mark| mark.to_s == ' '} == 2) || (board.grid.transpose[(position -1) % 3].count{|mark| mark.to_s == ' '} == 2) )
-          puts 4
-          choice = position
-          break
-        end
-      end
+      # [5, 1, 3, 7, 9, 2, 4, 6, 8].each do |position|
+      #   if position % 2 == 1 && board.get_cell(position).mark == ' '    && ((board.grid.each_with_index.map{|row, index| row[index]}.any?{ |mark| mark.to_s == @x_or_o} && (position == 1 || position == 9) && (board.grid.each_with_index.map{|row, index| row[index]}.count{|mark| mark.to_s == ' '} == 2)) || (board.grid.each_with_index.map{|row, index| row[2-index]}.any?{ |mark| mark.to_s == @x_or_o} && (position == 3 || position == 7) && (board.grid.each_with_index.map{|row, index| row[2-index]}.count{|mark| mark.to_s == ' '} == 2)))
+      #     puts 3
+      #   choice = position
+      #   break
+      #   #hor vert
+      #   elsif board.get_cell(position).mark == ' ' && (board.grid[(position -1) / 3].any?{|mark| mark.to_s == @x_or_o} || board.grid.transpose[(position -1) % 3].any?{|mark| mark.to_s == @x_or_o}  ) && ((board.grid[(position -1) / 3].count{|mark| mark.to_s == ' '} == 2) || (board.grid.transpose[(position -1) % 3].count{|mark| mark.to_s == ' '} == 2) )
+      #     puts 4
+      #     choice = position
+      #     break
+      #   end
+      # end
     end
     #none of above follows this simple routine
     unless choice
