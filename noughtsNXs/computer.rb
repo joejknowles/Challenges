16ba997a8edce4a791bd9b93d569b01cc9e0d11a
Joseph Knowles <joejknowles@gmail.com>
2015-03-10 22:48:04 +0000

    Add some final tactical edges

--- noughtsNXs/computer.rb
+++ noughtsNXs/computer.rb
@@ -19,6 +19,7 @@ class Computer < Player
   def initialize(settings_hsh)
     super(settings_hsh)
     @other_mark = settings_hsh.fetch(:other_mark, 'O')
+
   end
 
   def relevant_lines(cell, winning_lines)
@@ -28,15 +29,22 @@ class Computer < Player
   def choose_move(board, winning_lines)
     choice = nil
     sequence = [ 5, 1, 3, 7, 9, 2, 4, 6, 8]
+    # 4 tactical changes of plan
     sequence = [ 5, 9, 3, 7, 1, 2, 4, 6, 8] if board.get_cell(6).mark == @other_mark || board.get_cell(8).mark == @other_mark
+    sequence = [ 5, 9, 7, 3, 1, 2, 4, 6, 8] if board.get_cell(9).mark == @other_mark && board.get_cell(4).mark == @other_mark
+    sequence = [ 5, 7, 9, 3, 1, 2, 4, 6, 8] if board.get_cell(1).mark == @other_mark && board.get_cell(8).mark == @other_mark
+    sequence = [ 5, 2, 7, 9, 3, 1, 4, 6, 8] if (board.get_cell(1).mark == @other_mark && board.get_cell(9).mark == @other_mark) || (board.get_cell(3).mark == @other_mark && board.get_cell(7).mark == @other_mark)
+    choice = 3 if board.turn_count == 0
+    choice = 7 if board.turn_count == 2
+
     # This part checks for any winning moves
     sequence.each do |position|
-        cell = board.get_cell(position)
-        if cell.to_s == ' ' && relevant_lines(cell, winning_lines).any?{ |line| line.count{ |mark| mark.to_s == @x_or_o} == 2}
-          choice = position
-          break
-        end
+      cell = board.get_cell(position)
+      if cell.to_s == ' ' && relevant_lines(cell, winning_lines).any?{ |line| line.count{ |mark| mark.to_s == @x_or_o} == 2}
+        choice = position
+        break
       end
+    end
     #makes sure they're not about to win!
     unless choice
       sequence.each do |position|
