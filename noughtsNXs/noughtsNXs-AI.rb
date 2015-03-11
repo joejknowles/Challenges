94cf5654e901e0288f233b30da1d01c3a3eaab26
Joseph Knowles <joejknowles@gmail.com>
2015-03-11 22:26:21 +0000

    Separate GameSave class

--- noughtsNXs/noughtsNXs-AI.rb
+++ noughtsNXs/noughtsNXs-AI.rb
@@ -10,28 +10,31 @@ class Game
 
   def initialize
     @current_player, @other_player = start_game
-    @board = Board.new#(save)
+    @board = Board.new
     intro
     @begin = true
     next_turn
   end
 
   def intro
-    40.times{ print "#="}
-    puts""
-    40.times{ print "#="}
-    puts""
-    40.times{ print "#="}
-    puts""
+    print hash_lines
     puts "EXCELLENT! \nSo, #{@current_player.name}, you go first with '#{@current_player.x_or_o}' as your mark. #{@other_player.name}, your mark will be '#{@other_player.x_or_o}'.\nTo play, you need to type in a number between 1 and 9 to enter your mark onto the board.\nThe numbers you enter correspond to the positions shown here:\n"
     print_layout
     puts "This is your blank game board. Follow the instructions to start filling it in:"
   end
 
+  def hash_lines
+    "#{"#=" * 40}\n#{"#=" * 40}\n#{"#=" * 40}\n"
+  end
+
   def start_game
     puts "Would you like to start a new game or load a previous game? 'new'/'load'."
     load = get_input.downcase
-    load_game if load == 'load'
+    if load == 'load'
+      loader = GameSave.new
+      loader.show_files
+      loader.load_game(get_input.to_i - 1)
+    end
     puts "Please enter your name then press enter."
      #Weird running .shuffle first here?
     name_one = get_input ###################
@@ -42,7 +45,7 @@ class Game
   end
 
   def next_turn
-    print_board ################## Local variables removed as not necessary
+    print_board
     puts "Hey, #{@current_player.name}, it's your turn, you're '#{@current_player.x_or_o}'s. Put your '#{@current_player.x_or_o}' down on the board by choosing a number between 1 and 9. (Type 'help' or 'options' for a list of options)."
     @current_player.is_a?(Computer) ? position = @current_player.choose_move(@board, winning_lines) : position = get_input.to_i
     next_turn unless @board.mark_cell(position, @current_player.x_or_o)
@@ -52,16 +55,9 @@ class Game
   end
 
   def help
-    35.times{print"#"}################# method
-    print "HELP"
-    35.times{print"#"}
-    puts ""
+    print hash_lines
     puts "Type 'grid' or 'key' if you need reminding where the numbers go."
     puts "Type 'exit' or 'quit' to exit without saving, or 'save' to save your current game."
-    35.times{print"#"}
-    print "HELP"
-    35.times{print"#"}
-    puts ""
     next_turn
   end
 
@@ -69,11 +65,11 @@ class Game
     @current_player, @other_player = @other_player, @current_player
   end
 
-  def get_input########new class
+  def get_input######## new class
     print '>> '
     input = gets.chomp
     check_input = input.downcase
-    #######case nested in if
+    ####### case nested in if
     if check_input == 'grid' || check_input == 'key' || check_input == 'rules'
       puts "Just enter a number to put your mark in these places:"
       print_layout
@@ -84,7 +80,8 @@ class Game
     elsif @begin && (check_input == 'help' || check_input == 'options')
       help
     elsif @begin && check_input == 'save'
-      save_game
+      GameSave.new(self, file_name).save_game
+      end_game
     elsif @begin && check_input!= 'y' && check_input!= 'n' && (input.to_i > 9 || input.to_i < 1)
       puts "I don't understand that! Try again"
       check_end
@@ -92,6 +89,7 @@ class Game
     end
 
     input
+
   end
 
   def print_board
@@ -152,32 +150,64 @@ class Game
     end_game
   end
 
-  def save_game
+  def file_name
     file_name = "./game_saves/#{@current_player.name.split(' ').join('_')}_and_#{@other_player.name.split(' ').join('_')}_turn_#{@board.turn_count+1}"
-    file_name = file_name << '_again' while Dir['./game_saves/*.xml'].include?(file_name + '.xml')
-    file_name = file_name << ".xml"
-    puts "Saved as file: #{file_name[13..-1]}"
-    File.open(file_name, 'w'){|file| file.write(Ox.dump(self))}
-    puts "Your game has been successfully saved."
-    end_game
-  end
-
-  def load_game
-    puts "Choose from one of your save files by typing the number:"
-    all_save_files = Dir['./game_saves/*.xml']
-    if all_save_files.length == 0
-      puts "You don't have any saved games. Choose new game next time."
-      Game.new
-    end
-    all_save_files.each_with_index{|file, index| puts "#{index+1}: #{file[13..-1]}"}
-    load_choice = get_input.to_i - 1
-    game_save = all_save_files[load_choice]
-    game_save_xml = 0 #to make available after block, instancevariable better?
-    File.open(game_save, 'r'){|file| game_save_xml = file.read}
-    Ox.parse_obj(game_save_xml).next_turn
+    file_name << '_again' while Dir['./game_saves/*.xml'].include?(file_name + '.xml')
+    file_name << ".xml"
   end
 
 end
+
+#-------------------
+# GAME - SAVE
+#-------------------
+
+class GameSave
+
+  def initialize(game_data=nil, file_name=nil)
+    @game_data = game_data
+    @file_name = file_name
+    @all_save_files = Dir['./game_saves/*.xml']
+  end
+
+  def save_game
+    puts "Saved as file: #{@file_name[13..-1]}"
+    File.open(@file_name, 'w'){|file| file.write(Ox.dump(@game_data))}
+    puts "Your game has been successfully saved."
+  end
+
+  def show_files
+    puts "Choose from one of your save files by typing the number:"
+    if @all_save_files.length == 0
+      puts "You don't have any saved games. Choose new game next time."
+      Game.new
+    end
+    @all_save_files.each_with_index{|file, index| puts "#{index+1}: #{file[13..-1]}"}
+  end
+
+  def load_game(load_choice)
+    File.open(@all_save_files[load_choice], 'r'){|file| @game_save_xml = file.read}
+    Ox.parse_obj(@game_save_xml).next_turn
+  end
+
+end
+
+
+class Input
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 Game.new #.next_turn
